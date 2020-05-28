a9ea8e0d5d893c378a0d50e0e52770043500b382
semba.ayane <semba.ayane@revamp.co.jp>
2020-05-28 11:05:31 +0900

    components

---
+++ src/components/posts/sidebar.php
@@ -0,0 +1,14 @@
+<!-- 一般ユーザー用サイドバーです -->
+<div class="container">
+  <main class="main">
+    <!-- メインコンテンツ -->
+  </main>
+  <div class="sidebar">
+    <div class="sidebar__item">
+      <!-- 中身 -->
+    </div>
+    <div class="sidebar__item sidebar__item--fixed">
+      <!-- 固定・追従させたいエリア -->
+    </div>
+  </div>
+</div>

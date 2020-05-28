ffa63e08c23efb16a00536fa64c1ca7d81f7c312
semba.ayane <semba.ayane@revamp.co.jp>
2020-05-28 14:21:13 +0900

    header修正

--- src/pages/index.php
+++ src/pages/index.php
@@ -23,20 +23,3 @@
     </main>
 </body>
 </html>
-
-<!DOCTYPE html>
-<html>
-<head>
-         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-              <link href="http://localhost/css/system.css" rel="stylesheet" type="text/css" />
-     
-</head>
-<body>
-    <?php include('../components/admin/header.php'); ?>
-    <?php include('../components/admin/sidebar.php'); ?>
-    <main>
-        <!-- ここに内容を入れてください-->
-    </main>
-</body>
-</html>
-

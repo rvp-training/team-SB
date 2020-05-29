e9af16eab44b13731decf09ef4d921207e41fb7e
semba.ayane <semba.ayane@revamp.co.jp>
2020-05-29 15:28:27 +0900

    Create delete/confirm.php

--- src/pages/admin/delete/confirm.php
+++ src/pages/admin/delete/confirm.php
@@ -4,13 +4,50 @@
 <head>
          <meta http-equiv="content-type" content="text/html; charset=utf-8" />
               <link href="http://localhost/css/system.css" rel="stylesheet" type="text/css" />
+              <link href="http://localhost/css/users.css" rel="stylesheet" type="text/css" />
      
 </head>
 <body>
-    <?php include('../../../components/admin/header.php'); ?>
-    <?php include('../../../components/admin/sidebar.php'); ?>
-    <main>
-        <!-- ここに内容を入れてください-->
-    </main>
+    <div id="wrapper">
+        <?php include('../../../components/admin/header.php'); ?>
+        <?php include('../../../components/admin/sidebar.php'); ?>
+        <main>
+            
+            <?php
+                //GET /admin/users/delete API呼び出し
+                $curl = curl_init();
+                curl_setopt($curl, CURLOPT_URL, "http://web/api/admin/users/delete.php");
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                $response = curl_exec($curl);
+                $result = json_decode($response, true);
+            ?>
+            
+            <H3>このユーザーを削除して本当によろしいですか</H3>
+            
+            <table>
+            <tr>
+                <th>氏名</th>
+                <th>部署</th>
+                <th>役職</th>
+                <th>メールアドレス</th>
+            </tr>
+            <tr>
+            <td>
+                <?php print $result["name"];?> 
+            </td>
+            <td>
+                <?php print $result["department"];?> 
+            </td>
+            <td>
+                <?php print $result["position"];?> 
+            </td>
+            <td>
+                <?php print $result["mail"];?> 
+            </td>
+
+
+
+        </main>
+    </div>
 </body>
 </html>

1db9262f1b6e23b9d26733363f227e041e18a8e6
suzukiarisa <56010526+suzukiarisa@users.noreply.github.com>
2020-05-29 18:41:11 +0900

    update: ユーザー新規登録と確認画面
    新規登録画面
    ┗エラーメッセージが出る（空白と文字の長さのみ）
    
    確認画面
    ┗エラーがでている状態のため後程修正

--- src/pages/admin/register/confirm.php
+++ src/pages/admin/register/confirm.php
@@ -1,16 +1,59 @@
 
 <!DOCTYPE html>
 <html>
-<head>
-         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-              <link href="http://localhost/css/system.css" rel="stylesheet" type="text/css" />
-     
-</head>
-<body>
-    <?php include('../../../components/admin/header.php'); ?>
-    <?php include('../../../components/admin/sidebar.php'); ?>
-    <main>
-        <!-- ここに内容を入れてください-->
-    </main>
-</body>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <link href="http://localhost/css/system.css" rel="stylesheet" type="text/css" />  
+    </head>
+    <body>
+        <?php include('../../../components/admin/header.php'); ?>
+        <?php include('../../../components/admin/sidebar.php'); ?>
+        <main>
+            <title>この内容で登録してよろしいですか</title>
+            <?php
+                session_start();
+
+                if (!isset($_SESSION['register'])){
+                    header('Location: index.php');
+                    exit();
+                }       
+            ?>
+
+            <form action="" method="post">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>氏名</th>
+                            <th>部署</th>
+                            <th>役職</th>
+                            <th>メールアドレス</th>
+                            <th>パスワード</th>
+                        </tr>
+                    </thead>    
+                    <tbody>
+                        <tr>
+                            <th>
+                                <?php echo htmlspecialchars($_SESSION['register']['name'], ENT_QUOTES); ?>
+                            </th>
+                            <th>
+                                <?php echo htmlspecialchars($_SESSION['register']['department'], ENT_QUOTES); ?>
+                            </th>
+                            <th>
+                                <?php echo htmlspecialchars($_SESSION['register']['position'], ENT_QUOTES); ?>
+                            </th>
+                            <th>
+                                <?php echo htmlspecialchars($_SESSION['register']['mail'], ENT_QUOTES); ?>
+                            </th>
+                            <th>
+                                <?php echo htmlspecialchars($_SESSION['register']['pass'], ENT_QUOTES); ?>
+                            </th>
+                        </tr>
+                    </tbody>
+                </table>
+                <div>
+                    <a href="index.php?action=rewrite">&laquo;&nbsp;戻る</a> | <input type="submit" value="登録" />
+                </div>
+            </form>               
+        </main>
+    </body>
 </html>
--- src/pages/admin/register/index.php
+++ src/pages/admin/register/index.php
@@ -1,16 +1,98 @@
+<?php
+    session_start();
+
+    if (!empty($_POST)){
+    // エラーの確認項目
+    if ($_POST['name'] == ''){
+        $error['name'] = 'blank';
+    }
+    if ($_POST['department'] == ''){
+        $error['department'] = 'blank';
+    }
+    if ($_POST['position'] == ''){
+        $error['position'] = 'blank';
+    }
+    if ($_POST['mail'] == ''){
+        $error['mail'] = 'blank';
+    }
+    if (strlen($_POST['pass']) < 6){
+        $error['pass'] = 'length';
+    }
+
+    if (empty($error)){
+        $_SESSION['register'] = $_POST;
+        header('Location: confirm.php');
+        exit();
+    }
+}
+
+    // 書き直し
+    if ($_REQUEST['action'] == 'rewrite'){
+        $_POST = $_SESSION['register'];
+        $error['rewrite'] = true;
+    }
+?>
 
 <!DOCTYPE html>
 <html>
-<head>
-         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-              <link href="http://localhost/css/system.css" rel="stylesheet" type="text/css" />
-     
-</head>
-<body>
-    <?php include('../../../components/admin/header.php'); ?>
-    <?php include('../../../components/admin/sidebar.php'); ?>
-    <main>
-        <!-- ここに内容を入れてください-->
-    </main>
-</body>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <link href="http://localhost/css/system.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <?php include('../../../components/admin/header.php'); ?>
+        <?php include('../../../components/admin/sidebar.php'); ?>
+        <main>
+        <p>ユーザーの新規登録</p>
+            <form action="" method="post" enctype="multipart/form-data">
+                <dl>
+                    <dt>氏名</dt>
+                    <dd>
+                        <input type="text" name="name" size="35" maxlength="30" value="<php? echo htmlspecialchars($_POST['name'], ENT_QUOTES); ?>" />
+                        <?php if ($error['name'] == 'blank'): ?>
+                            <p class="register-error-message">* 必須項目です</p>
+                        <?php endif; ?>
+                    </dd>
+
+                    <dt>部署</dt>
+                    <dd>
+                        <input type="text" name="department" size="35" maxlength="15" value="<php? echo htmlspecialchars($_POST['department'], ENT_QUOTES); ?>" />
+                        <?php if ($error['department'] == 'blank'): ?>
+                            <p class="register-error-message">* 必須項目です</p>
+                        <?php endif; ?>
+                    </dd>
+
+                    <dt>役職</dt>
+                    <dd>
+                        <input type="text" name="position" size="35" maxlength="15" value="<php? echo htmlspecialchars($_POST['position'], ENT_QUOTES); ?>" />
+                        <?php if ($error['position'] == 'blank'): ?>
+                            <p class="register-error-message">* 必須項目です</p>
+                        <?php endif; ?>
+                    </dd>
+
+                    <dt>メールアドレス</dt>
+                    <dd>
+                        <input type="text" name="mail" size="35" maxlength="45" value="<php? echo htmlspecialchars($_POST['mail'], ENT_QUOTES); ?>" />
+                        <?php if ($error['mail'] == 'blank'): ?>
+                            <p class="register-error-message">* メールアドレスは英数字または記号で入力してください</p>
+                        <?php endif; ?>
+                    </dd>
+
+                    <dt>パスワード</dt>
+                    <dd>
+                        <input type="text" name="pass" size="35" maxlength="45" value="<php? echo htmlspecialchars($_POST['pass'], ENT_QUOTES); ?>" />
+                        <?php if ($error['pass'] == 'blank'): ?>
+                            <p class="register-error-message">* 必須項目です</p>
+                        <?php endif; ?>
+                        <?php if ($error['pass'] == 'length'): ?>
+                            <p class="register-error-message">* パスワードは8文字以上の英数字または記号で入力してください</p>
+                        <?php endif; ?>
+                    </dd>
+                </dl>
+                <div>
+                    <input type="submit" value="登録" />
+                </div>
+            </form>
+        </main>
+    </body>
 </html>

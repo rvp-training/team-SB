82482e3e921b8df0bd116c433d507650df74734b
komiya.daiki <komiya.daiki@revamp.co.jp>
2020-06-01 10:43:18 +0900

    update

--- src/api/admin/users/index.php
+++ src/api/admin/users/index.php
@@ -1,18 +1,15 @@
 <?php
 
-
 header('Content-Type: text/json; charset=UTF-8');
 
 //DBと接続
 try{
     $dbh = new PDO('pgsql:dbname=postgres;host=db','postgres','password');
-     
-} catch(PDOException $e){
+     } catch(PDOException $e){
     echo 'DB接続エラー; ' . $e->getMessage();
 }
-//変数にDBからとってきたデータを配列として格納
 
-
+//DBからとってきたデータを配列として格納
 $prepare = $dbh->prepare('SELECT * FROM users;');
 
 $prepare->execute();
@@ -23,15 +20,5 @@ $jsonstr =  json_encode($result, JSON_UNESCAPED_UNICODE);
 
 echo $jsonstr;
 
-
-/*$result = $prepare ->fetch();
-
-$jsonstr =  json_encode($result, JSON_UNESCAPED_UNICODE
-);
- 
-
-
-echo $jsonstr;
-*/
 ?>
   

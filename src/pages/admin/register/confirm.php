0ff5ac62b7e36dccac0e10236324516c6c18cc74
suzukiarisa <56010526+suzukiarisa@users.noreply.github.com>
2020-06-03 14:47:32 +0900

    update: 確認画面のコードを削除
    確認画面の戻るボタンで新規登録画面に戻った際、入力していた内容が引き継ぎできなくなった

--- src/pages/admin/register/confirm.php
+++ src/pages/admin/register/confirm.php
@@ -1,10 +1,10 @@
 <?php
     session_start();
 
-    if (!isset($_SESSION['register'])){
-        header('Location: index.php');
-        exit();
-    }       
+    // if (!isset($_SESSION['register'])){
+    //     header('Location: index.php');
+    //     exit();
+    // }       
 ?>
 
 <!DOCTYPE html>

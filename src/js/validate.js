4dc81275cb73cc695ede560287723e98186100d1
suzukiarisa <56010526+suzukiarisa@users.noreply.github.com>
2020-06-05 13:49:33 +0900

    新規登録画面バリデーション
    作動しません

---
+++ src/js/validate.js
@@ -0,0 +1,56 @@
+$(function() {
+    $("#userNewForm").validate({
+        errorElement: "span",
+        errorClass: "alert",
+        rules: {
+            name: {
+                required: true,
+                maxlength: 30
+            },
+            department: {
+                required: true,
+                maxlength: 15
+            },
+            position: {
+                required: true,
+                maxlength: 15
+            },
+            mail: {
+                required: true,
+                email : true,
+                maxlength: 45
+            },
+            pass: {
+                required: true,
+                maxlength: 45
+            },
+        },
+
+    messages: {
+        name: {
+            required: "氏名を入力してください",
+            maxlength: "氏名は30文字以内で入力してください"
+        }
+    },
+        department: {
+            required: "部署を入力してください",
+            maxlength: "部署は15文字以内で入力してください"
+        }
+    },
+        position: {
+            required: "役職を入力してください",
+            maxlength: "役職は15文字以内で入力してください"
+        }
+    },
+        mail: {
+            required: "メールアドレスを入力してください",
+            email : "正式なメールアドレスを入力してください",
+            maxlength: "メールアドレスは45文字以内で入力してください"
+        }
+    },
+        pass: {
+            required: "パスワードを入力してください",
+            maxlength: "パスワードは45文字以内で入力してください"
+        }
+    }
+});
--- src/pages/login/index.php
+++ src/pages/login/index.php
@@ -1,3 +1,19 @@
+<?php
+    session_start();
+
+    if (!empty($_POST)){
+        // エラーの確認項目
+        // if ($_POST['name'] === ''){
+        //     $error['name'] = 'blank';
+        // }
+        if (strlen($_POST['pass']) < 8){
+            $error['pass'] = 'length';
+        }
+        if ($_POST['pass'] === ''){
+            $error['pass'] = 'blank';
+        }
+    }
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,8 +31,17 @@
                 } else {
                     print "メールアドレスを入力してください";
                 }?>"></p>
+
                 <label for="pass" class="login_text">パスワード</label>
                 <p><input class="form-text" type="text" name="pass" maxlength="45" placeholder="パスワードを入力してください"></p>
+
+                <?php if ($error['pass'] === 'blank'): ?>
+                    <p class="error-message">* 必須項目です</p>
+                <?php endif; ?>
+                <?php if ($error['pass'] === 'length'): ?>
+                    <p class="error-message">* パスワードは8文字以上の英数字または記号で入力してください</p>
+                <?php endif; ?>
+
                 <p><input id="login_button" type="submit" value="ログイン"></p>
             </form>
             <p>(管理者の方は<a href="http://localhost/pages/login/admin">こちら</a>)</p>

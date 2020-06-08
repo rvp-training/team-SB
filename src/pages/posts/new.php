83fdae73cd4dd8a77e46dcdc618868d9e400d733
semba.ayane <semba.ayane@revamp.co.jp>
2020-06-08 17:33:29 +0900

    バリデーション機能を追加・CSSの修正

--- src/pages/posts/new.php
+++ src/pages/posts/new.php
@@ -11,25 +11,28 @@
     <main>
         <form action="http://localhost/api/posts/new" method="post" enctype="multipart/form-data">       
             <p>送信ファイル</p>
+            <input type="file" name="image[]" multiple = "multiple" required>
             <input type="file" name="image[]" multiple = "multiple" >
             <input type="file" name="image[]" multiple = "multiple" >
             <input type="file" name="image[]" multiple = "multiple" >
             <input type="file" name="image[]" multiple = "multiple" >
-            <input type="file" name="image[]" multiple = "multiple" >
+                <br>
+                <dl>
+
+            <dt>タイトル</dt>
+            <dd><input class="form-input" type="text" name="title" maxlength="30" placeholder="タイトルを入力してください" required></input></dd>
             <br>
-            <p>タイトル</p>
-            <input class="form-input" type="text" name="title" placeholder="タイトルを入力してください"></input>
+            <dt>テキスト</dt>
+            <dd><input class="form-input" type="text" name="text" maxlength="1000" placeholder="テキストを入力してください"></input></dd>
             <br>
-            <p>テキスト</p>
-            <input class="form-input" type="text" name="text" placeholder="テキストを入力してください"></input>
-            <br>
-            <p>タグ</p>
-            <input class="form-input" type="text" name="tag" placeholder="タグを入力してください"></input>
-            <br><br>
+            <dt>タグ</dt>
+            <dd><input class="form-input" type="text" name="tag" maxlength="90" placeholder="タグを入力してください"></input></dd>
+        </dl>
+               <br>
             <input type="radio" name="category_id" value="1" checked>ビジネス</input>
             <input type="radio" name="category_id" value="2">プライベート</input>  
             <br><br>
-            <input type="submit" name="upload" value="投稿"></input>
+            <input type="submit" class="post_button" name="upload" value="投稿"></input>
         </form>
     </main>
 </body>

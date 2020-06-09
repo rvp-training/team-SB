86315de843937d237fa5952372f93508c65639c0
semba.ayane <semba.ayane@revamp.co.jp>
2020-06-09 14:49:30 +0900

    サイドバーのリンクを修正・不要なファイルの削除

--- src/components/admin/sidebar.php
+++ src/components/admin/sidebar.php
@@ -2,12 +2,12 @@
 <div class="sidebar">
     <div class="sidebar_items">
         <ul>
-            <li class="sidebar_item"><a class="sidebar_link" href="../../pages/admin/index.php">トップ</a></li>
-            <li class="sidebar_item"><a class="sidebar_link" href="../../pages/admin/users.php">ユーザー一覧</a></li>
-            <li class="sidebar_item"><a class="sidebar_link" href="../../pages/admin/register/index.php">新規登録</a></li>
+            <li class="sidebar_item"><a class="sidebar_link" href="http://localhost/pages/admin/index.php">トップ</a></li>
+            <li class="sidebar_item"><a class="sidebar_link" href="http://localhost/pages/admin/users.php">ユーザー一覧</a></li>
+            <li class="sidebar_item"><a class="sidebar_link" href="http://localhost/pages/admin/register/index.php">新規登録</a></li>
         </ul>
         <div class="sidebar_item sidebar_item_fixed">
-            <a class="sidebar_link" href="../logout/index.php">ログアウト</a>
+            <a class="sidebar_link" href="http://localhost/logout/index.php">ログアウト</a>
         </div>
     </div>
 </div>

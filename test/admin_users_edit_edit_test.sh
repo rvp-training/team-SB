27a6e8892af392c0fdc25af8bb5f7a78b59a362d
semba.ayane <semba.ayane@revamp.co.jp>
2020-06-15 18:13:32 +0900

    Update admin_users_edit_edit_test.sh

--- test/admin_users_edit_edit_test.sh
+++ test/admin_users_edit_edit_test.sh
@@ -1,3 +1,3 @@
-curl http://localhost/api/admin/users/edit/edit_test \
+curl http://localhost/api/admin/users/edit/edit \
     -X POST \
-      -d 'department=%E4%B8%80%E8%88%AC%E3%83%86%E3%82%B9%E3%83%882&position=%E4%B8%80%E8%88%AC%E3%83%86%E3%82%B9%E3%83%882&mail=generaltest2@co.jp&pass=generaltest'
+      -d 'id=2&name=%E7%94%BB%E9%9D%A2%E3%83%86%E3%82%B9%E3%83%881&department=%E9%83%A8%E7%BD%B21&position=%E5%BD%B9%E8%81%B71&mail=test@co.jp&pass=password'

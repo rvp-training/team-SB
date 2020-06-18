e3d72f1a0a67a48d8b0b54945f26c1d4b43f5367
semba.ayane <semba.ayane@revamp.co.jp>
2020-06-18 10:22:48 +0900

    相対パス

--- src/pages/login/index.php
+++ src/pages/login/index.php
@@ -3,7 +3,7 @@
 <html>
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <link href="http://localhost/css/system.css" rel="stylesheet" type="text/css" />
+    <link href="../../css/system.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
     <div id="login_bg">
@@ -12,7 +12,7 @@
         <p class="login_message">&emsp;<?php echo $_SESSION['login_message'];?>&emsp;</p>
         <?php unset( $_SESSION['login_message'] );?>
         <?php endif; ?>
-            <form action="http://localhost/login/index.php" method="POST">
+            <form action="../../login/index.php" method="POST">
                 <label for="mail" class="login_text">メールアドレス</label>
                 <p><input class="form-text" type="email" name="mail" maxlength="45" placeholder="メールアドレスを入力してください"
                 <?php if (isset($_SESSION["mail"])): ?>
@@ -24,7 +24,7 @@
                 <p><input class="form-text" type="text" name="pass" minlength="8" maxlength="45" pattern="^[a-zA-Z0-9-_]+$" placeholder="パスワードを入力してください" required></p>   
                 <p><input id="login_button" type="submit" value="ログイン"></p>
             </form>
-            <p>(管理者の方は<a href="http://localhost/pages/login/admin">こちら</a>)</p>
+            <p>(管理者の方は<a href="./admin">こちら</a>)</p>
         </div>
     </div>
 </body>

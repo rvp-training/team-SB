390ded866973b410fb511b7dc07944d9ac27d079
semba.ayane <semba.ayane@revamp.co.jp>
2020-06-19 10:17:20 +0900

    DBに投稿を追加するコマンド

---
+++ test/post/new/new.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+curl http://localhost/api/posts/new \
+    -X POST \
+    -H 'Content-Type:multipart/form-data' \
+    -F 'image[]=@./images/5eec0fd4c9591.png' \
+    -F 'image[]=@./images/5eec0fd4c9591.png' \
+    -F 'image[]=@./images/5eec0fd4c9591.png' \
+    -F 'image[]=@./images/5eec0fd4c9591.png' \
+    -F 'image[]=@./images/5eec0fd4c9591.png' \
+    -F 'upload=Upload' \
+    -F 'title=Title' \
+    -F 'text=Text' \
+    -F 'tag=Tag' \
+    -F 'category_id=1'
+
+docker container exec team-sb_db_1 psql -U postgres -c 'SELECT * FROM images ORDER BY id DESC LIMIT 1'
+docker container exec team-sb_db_1 psql -U postgres -c 'SELECT * FROM posts ORDER BY id DESC LIMIT 1'

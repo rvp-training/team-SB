3d906163d76c9c830ad4a02065d3a5e134087f2e
semba.ayane <semba.ayane@revamp.co.jp>
2020-06-19 13:51:35 +0900

    ダミーデータを作成＆CSS修正

--- new.sh
+++ new.sh
@@ -7,15 +7,11 @@ curl http://localhost/api/posts/new \
     -X POST \
     -H 'Content-Type:multipart/form-data' \
     -F 'image[]=@./images/5eec0fd4c9591.png' \
-    -F 'image[]=@./images/5eec0fd4c9591.png' \
-    -F 'image[]=@./images/5eec0fd4c9591.png' \
-    -F 'image[]=@./images/5eec0fd4c9591.png' \
-    -F 'image[]=@./images/5eec0fd4c9591.png' \
+    -F 'image[]=dev/null;filename=' \
+    -F 'image[]=dev/null;filename=' \
+    -F 'image[]=dev/null;filename=' \
+    -F 'image[]=dev/null;filename=' \
     -F 'upload=Upload' \
     -F 'title=Title' \
-    -F 'text=Text' \
-    -F 'tag=Tag' \
+    -F 'user_id=1' \
     -F 'category_id=1'
-
-docker container exec team-sb_db_1 psql -U postgres -c 'SELECT * FROM images ORDER BY id DESC LIMIT 1'
-docker container exec team-sb_db_1 psql -U postgres -c 'SELECT * FROM posts ORDER BY id DESC LIMIT 1'
